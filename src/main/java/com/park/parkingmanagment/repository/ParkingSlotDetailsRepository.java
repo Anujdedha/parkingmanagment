9a32e8a8ae7b7b23df0da3e263146239c8f493e1
anuj3.kumar <dedhaanuj@gmail.com>
2020-08-20 00:04:26 +0530

    commit for remove extra import

--- src/main/java/com/park/parkingmanagment/repository/ParkingSlotDetailsRepository.java
+++ src/main/java/com/park/parkingmanagment/repository/ParkingSlotDetailsRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.test.context.jdbc.Sql;
+
 
 import com.park.parkingmanagment.model.ParkingSlotDetails;
 @Repository
